738258eb645007cfd4bb0e341b8b2e66759afa0d
gempir <gempir.dev@gmail.com>
2023-03-29 15:13:59 +0200

    code link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { CodeBracketIcon } from '@heroicons/react/24/solid'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -5,6 +6,9 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <a href="https://github.com/gempir/gptchat" className='top-5 right-5 absolute hover:opacity-50' target={"_blank"}>
+          <CodeBracketIcon className='h-6'/>
+        </a>
         <Main />
         <NextScript />
       </body>
